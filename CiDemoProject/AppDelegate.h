c04e4c4ee0dea8ed8dd41b607f6faac3fbbed4a1
Fengbo <342077837@qq.com>
2016-12-20 13:44:19 +0800

    初始化上传

---
+++ CiDemoProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CiDemoProject
+//
+//  Created by Fengbo on 16/12/20.
+//  Copyright © 2016年 Fengbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CiDemoProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CiDemoProject
+//
+//  Created by Fengbo on 16/12/20.
+//  Copyright © 2016年 Fengbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
